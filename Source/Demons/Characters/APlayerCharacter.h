ae028b2ac91663736a12a1a70a7259c21fca6e44
Dan <d.szabo87@gmail.com>
2024-01-17 01:00:54 +1100

    Begin setting up GAS

---
+++ Source/Demons/Characters/APlayerCharacter.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ACharacterBase.h"
+#include "APlayerCharacter.generated.h"
+
+UCLASS()
+class DEMONS_API APlayerCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+public:
+	
+};
